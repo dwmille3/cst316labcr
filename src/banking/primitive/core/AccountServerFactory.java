993ac850c54d4d0f6c103043c7d414ccbc873a72
dwmille3 <dwmille3@asu.edu>
2016-02-22 04:42:27 -0700

    Used to create and properly initialize account servers.

--- src/banking/primitive/core/AccountServerFactory.java
+++ src/banking/primitive/core/AccountServerFactory.java
@@ -1,6 +1,16 @@
 package banking.primitive.core;
 
+/*
+  File:	AccountServerFactory.java
+  Author:   ??
+  Date:	2/19/2016
+  Description: Used to create and properly initialize account servers.
+*/
 
+/**
+  Class:	AccountServerFactory
+  Description: An account server factory that wraps around a singleton.
+*/
 public class AccountServerFactory {
 
 	protected static AccountServerFactory singleton = null;
@@ -17,6 +27,11 @@ public class AccountServerFactory {
 		return singleton;
 	}
 
+	/**
+      Method: lookup
+      Returns: AccountServer
+      Description: Returns a new instance of ServerSolution.
+    */
 	public AccountServer lookup() {
 		return new ServerSolution();
 	}
